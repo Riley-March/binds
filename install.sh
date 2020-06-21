89d1c1ab009ff7d654d1c85d43ec0967e62ab2bb
Riley March <rileymarch@rileys-mbp.lan>
2020-06-21 21:56:00 +1000

    Fixes

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 ln -sf "$PWD"/.bashrc ~/.bashrc
-ln -sf "$PWD"/.bash_profile.sh ~/.bash_profile
+ln -sf "$PWD"/.bash_profile ~/.bash_profile
 ln -sf "$PWD"/.tmux.conf ~/.tmux.conf
-ln -sf "$PWD"/settings.json $HOME/Library/Application Support/Code/User/settings.json
+ln -sf "$PWD"/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
 
 source ~/.bashrc
 source ~/.bash_profile 
-source ~/.tmux.conf 
-source $HOME/Library/Application Support/Code/User/settings.json
+source ~/.tmux.conf
