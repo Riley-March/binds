dd44174e63fd318b8c6eef13eb150ba4e8fe0c60
Riley-March <riley.j.march@outlook.com>
2021-02-06 15:08:57 +1000

    fixes

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+
 mkdir ~/.config/neovim
 ln -sf "$PWD"/init.vim ~/.config/neovim/init.vim
 ln -sf "$PWD"/coc-settings.json ~/.config/neovim/coc-settings.json
-ln -sf "$PWD"/.tmux.conf ~/.tmux.conf
 
-source ~/.tmux.conf
+ln -sf "$PWD"/.zshrc ~/.zshrc
+
+source ~/.zshrc
