c6b3b445ed6624cb4258ec20efd43cc75de61e74
Riley-March <riley.j.march@outlook.com>
2021-02-06 15:47:26 +1000

    fixes

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-mkdir ~/.config/neovim
-ln -sf "$PWD"/init.vim ~/.config/neovim/init.vim
-ln -sf "$PWD"/coc-settings.json ~/.config/neovim/coc-settings.json
+mkdir ~/.config/nvim
+ln -sf "$PWD"/init.vim ~/.config/nvim/init.vim
+ln -sf "$PWD"/coc-settings.json ~/.config/nvim/coc-settings.json
 
 ln -sf "$PWD"/.zshrc ~/.zshrc
 
 source ~/.zshrc
+source ~/.config/nvim/init.vim
